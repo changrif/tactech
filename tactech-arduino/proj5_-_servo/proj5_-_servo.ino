2c1480f03dd8b4988205f4cc495b20b27f818890
Beatrice Villanueva <bcvillanueva96@gmail.com>
2019-10-16 16:17:54 -0400

    Adding code for Serial monitor parsing

--- tactech-arduino/proj5_-_servo/proj5_-_servo.ino
+++ tactech-arduino/proj5_-_servo/proj5_-_servo.ino
@@ -1,3 +1,7 @@
+// [[0, 1, 1, 1, 1, 0], [1, 0, 0, 0, 1, 0], [0, 1, 1, 1, 0, 0], [0, 1, 1, 1, 1, 0]]
+// [0, 1, 1, 1, 1, 0] # = 18
+/*
+NOTE: Maps and moves servo to a potentiometer
 #include <Servo.h>
 
 Servo myServo;
@@ -21,4 +25,75 @@ void loop(){
   Serial.println(angle);
   myServo.write(angle);
   delay(15);
+}
+*/
+// -------------
+/*
+void setup() {
+  Serial.begin(9600); // Initialize the serial connection at 9600 bits per second
+}
+
+char rx_byte = 0;
+
+void loop() {
+  if (Serial.available() > 0) {    // is a character available?
+    rx_byte = Serial.read();       // get the character
+  
+    // check if a number was received
+    if ((rx_byte >= '0') && (rx_byte <= '9')) {
+      Serial.print("Number received: ");
+      Serial.println(rx_byte);
+    }
+    else {
+      Serial.println("Not a number.");
+    }
+  } // end: if (Serial.available() > 0)
+}*/
+
+// Example 2 - Receive with an end-marker
+
+const byte numChars = 19;       //
+char receivedChars[numChars];   // an array to store the received data
+
+boolean newData = false;
+
+void setup() {
+    Serial.begin(9600);
+    Serial.println("<Arduino is ready>");
+}
+
+void loop() {
+    recvWithEndMarker();
+    showNewData();
+}
+
+void recvWithEndMarker() {
+    static byte ndx = 0;
+    char endMarker = '\n';
+    char rc;
+   
+    while (Serial.available() > 0 && newData == false) {
+        rc = Serial.read();
+
+        if (rc != endMarker) {
+            receivedChars[ndx] = rc;
+            ndx++;
+            if (ndx >= numChars) {
+                ndx = numChars - 1;
+            }
+        }
+        else {
+            receivedChars[ndx] = '\0'; // terminate the string
+            ndx = 0;
+            newData = true;
+        }
+    }
+}
+
+void showNewData() {
+    if (newData == true) {
+        Serial.print("This just in ... ");
+        Serial.println(receivedChars);
+        newData = false;
+    }
 }
